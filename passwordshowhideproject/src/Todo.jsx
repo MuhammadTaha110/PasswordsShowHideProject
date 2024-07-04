52897235cd86519fbe9adcb42342d45a9379b4ac
Muhammad Taha Talib <bcsbs2212188@szabist.pk>
2024-07-04 14:01:03 +0500

    working

--- passwordshowhideproject/src/Todo.jsx
+++ passwordshowhideproject/src/Todo.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './Todo.css';
 export let Todo = function (){
+
+    let [todo,setTodo] = useState([]);
+
+    let saveTodoList =(event) =>{
+
+        let value = event.target.todoinput.value;
+
+        if(todo.includes(value)){
+            alert('inserted');
+        }
+
+        else{
+            let finalTodo = [...todo,value];
+            alert(finalTodo)
+        }
+
+       
+        event.preventDefault();
+    }
 
     return(
         <div className="todo-outer-div">
             <h1>Todo App</h1>
+            <form onSubmit={saveTodoList}>
+                <input type="text" name='todoinput' />
+                <button>Save</button>
+            </form>
         </div>
     )
 
